5cbf5ec5c8ddd8194dee8a979ead31079dec2549
semlisa1990 <semlisa1990@gmail.com>
2023-09-16 10:36:20 +0300

    feat: solved

--- src/index.js
+++ src/index.js
@@ -39,8 +39,34 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let res = '';
+    for (let i = 0; i < expr.length; i += 10) {
+        let letter = expr.slice(i, i + 10);
+        if (letter == '**********') {
+            res += ' ';
+        } else {
+            let morseLetter = '';
+            for (let k = 0; k < 5; k++) {
+                let morseChar = letter.slice(k * 2, k * 2 + 2);
+                switch (morseChar) {
+                    case '10':
+                        morseLetter += '.';
+                        break;
+                    case '11':
+                        morseLetter += '-';
+                        break;
+                    default:
+                        break;
+                }
+            }
+            res += MORSE_TABLE[morseLetter];
+        }
+    }
+   return res;
 }
 
 module.exports = {
     decode
 }
+
+
